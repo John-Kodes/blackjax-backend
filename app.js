18679546d0c205af142a9e97b0a726dfce8a92ca
John-Kodes <jdanielsemine@gmail.com>
2021-10-18 17:49:30 +0400

    CORS implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -18,6 +19,9 @@ const app = express();
 app.enable("trust proxy");
 
 // GLOBAL MIDDLEWARES ____________________
+app.use(cors());
+
+app.options("*", cors());
 
 // Setting view engine
 app.set("view engine", "pug");
