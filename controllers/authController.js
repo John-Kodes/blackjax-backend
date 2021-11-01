58350bfdfbca6ae5e106831b5b96629a44d1ea62
John-Kodes <jdanielsemine@gmail.com>
2021-11-01 16:30:32 +0400

    /isLoggedIn route added for checking if a user is logged in by verifying their jwt and sending back user data

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,6 +37,47 @@ const createSendToken = (user, statusCode, req, res) => {
   });
 };
 
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it exists
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser)
+    return next(
+      new AppError("The user belonging to this token no longer exists", 401)
+    );
+
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password! Please log in again", 401)
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { user: currentUser },
+  });
+});
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     username: req.body.username,
