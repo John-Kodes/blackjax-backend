4e77a7998e0fb28e0ce1518477e761642a401d40
John-Kodes <jdanielsemine@gmail.com>
2021-11-01 17:24:07 +0400

    bugfix: /isLoggedIn from PATCH to GET method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.patch("/isLoggedIn", authController.isLoggedIn);
+router.get("/isLoggedIn", authController.isLoggedIn);
 router.get("/logout", authController.logout);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
